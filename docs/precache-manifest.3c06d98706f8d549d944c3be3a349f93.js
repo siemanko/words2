e4a55dc0be09c4882e98ee83fbc9c6a5e821ea1e
Szymon Sidor <szymon@openai.com>
2020-03-19 17:06:57 -0700

    deploy

---
+++ docs/precache-manifest.3c06d98706f8d549d944c3be3a349f93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943c9e42dcd4f263988fa9e0837681a9",
+    "url": "/words2/index.html"
+  },
+  {
+    "revision": "5cb74f3ca3a737470e44",
+    "url": "/words2/static/css/main.48be3dab.chunk.css"
+  },
+  {
+    "revision": "b96a61a81d568376c593",
+    "url": "/words2/static/js/2.906fb7db.chunk.js"
+  },
+  {
+    "revision": "0c587fe94bf84d3cee5677beacb48e8a",
+    "url": "/words2/static/js/2.906fb7db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5cb74f3ca3a737470e44",
+    "url": "/words2/static/js/main.ae3b6e8b.chunk.js"
+  },
+  {
+    "revision": "7a56473954ecdb3a2a02",
+    "url": "/words2/static/js/runtime-main.9091a3c6.js"
+  }
+]);
